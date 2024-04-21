e5ccc56c1011faf84138c3064ab61a188ab34191
𝘧𝘦𝘯 <69437909+fluffeon@users.noreply.github.com>
2024-04-21 13:25:24 -0300

    Diccionarios

---
+++ escuela.py
@@ -0,0 +1,29 @@
+#!/bin/python
+listaDeAlumnos={
+
+    "Juan":{
+        "Nombre": "Juan Santiago",
+        "Apellido": "Abregú",
+        "DNI": "123456789",
+        "Nacimiento": "26/02/2006",
+        "Grado": "5°",
+        "Burbuja": "A",
+        "Notas": [5, 6, 10, 8],
+        "Tutor": "Mi mama",
+        "Faltas": 0,
+        "Amonestaciones": 0},
+
+    "Angel":{
+        "Nombre": "Angel Nicolás",
+        "Apellido": "Flores Castro",
+        "DNI": "123456789",
+        "Nacimiento": "26/02/2006",
+        "Grado": "5°",
+        "Burbuja": "A",
+        "Notas": [5, 6, 10, 8],
+        "Tutor": "Mi mama",
+        "Faltas": 0,
+        "Amonestaciones": 0}
+    
+    }
+
